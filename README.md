fe6eeb78f58b19d3bbc9775fe8900d24aab1ebe6
Wilson Chan <wilsonchan54@gmail.com>
2023-09-23 10:34:47 -0400

    Added configurations in tailwind.config.ts / added roboto font in global.css / updated README.md

--- README.md
+++ README.md
@@ -10,3 +10,29 @@ npx shadcn-ui@latest add button
 npm install @sanity/image-url @sanity/vision query-string sanity
 
 -->
+
+
+<!-- PROJECT LOGO -->
+<br />
+<div align="center" id='readme-top'>
+  <img src="https://i.makeagif.com/media/5-12-2016/8btHfN.gif" alt="Logo" width="300" height="10">
+
+  <h3 align="center">Thermo</h3>
+
+  <p align="center">
+    Template optimized speed and SEO
+    <br />
+    <a href="https://github.com/WackyChomp/thermo"><strong>Explore the docs »</strong></a>
+    <br />
+    <br />
+    <a href="https://github.com/WackyChomp/thermo">View Demo</a>
+    ·
+    <a href="https://github.com/WackyChomp/thermo/issues">Report Bug</a>
+    ·
+    <a href="https://github.com/WackyChomp/thermo/issues">Request Feature</a>
+  </p>
+  
+  <img src="https://i.makeagif.com/media/5-12-2016/8btHfN.gif" alt="Logo" width="300" height="10">
+</div>
+
+
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,14 +5,55 @@ const config: Config = {
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/**/*.{ts,tsx}',
   ],
   theme: {
+    /* ----- Screens ----- */
+    screens: {
+      xs: '400px',
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1220px',
+      '2xl': '1440px',
+      '3xl': '1700px',
+    },
+
+    /* ----- Container ----- */
+    container: {
+      center: true,
+      padding: '2rem',
+      screens: {
+        '2xl': '1440px',
+      },
+    },
+
+    /* ----- Extend ----- */
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      fontFamily: {
+        roboto: ['roboto', 'sans-serif'],
+      },
+      colors: {
+        primary: "orangered",
+        black: {
+          DEFAULT: "#000",
+          100: "#0D1117",
+          200: "#161B22",
+          300: "#1F2428",
+          400: "#242C38",
+        },
+        white: {
+          DEFAULT: "#FFF",
+          400: "#A3B3BC",
+          500: "#A4B8D5",
+          800: "#D0DFFF",
+        },
+      }
     },
   },
   plugins: [],
