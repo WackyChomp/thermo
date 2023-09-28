d2e0db488768bc0709ee282b35648698f4aad3c5
Wilson Chan <wilsonchan54@gmail.com>
2023-09-27 20:20:39 -0400

    Added banner and adjusted padding in home route group page.tsx / added styling to globals.css

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -5,24 +5,38 @@ import { Button } from '@/components/ui/button'
 
 export default function Home() {
   return (
-    <div>
-      <div className="bg-yellow-300 pt-20">
-        <h1 className='text-2xl underline'>Home Child 1</h1>
-        <div className="flex p-3 space-x-3 flex-col">
-          <Button className='bg-blue-400'>1-Click Me</Button>
-          <Button className='bg-red-400'>2-Click Me</Button>
-          <Button className='bg-green-400'>3-Click Me</Button>
-          <Button className='bg-purple-400'>4-Click Me</Button>
-        </div>
+    <main>
 
-        <h1 className='text-2xl underline'>Home Child 2</h1>
-        <div className="grid p-3 gap-3 grid-rows-2 grid-flow-col">
-          <Button className='bg-blue-400'>1-Click Me</Button>
-          <Button className='bg-red-400'>2-Click Me</Button>
-          <Button className='bg-green-400'>3-Click Me</Button>
-          <Button className='bg-purple-400'>4-Click Me</Button>
+      <div className="flex-center mx-auto w-full max-w-screen-2xl flex-col pt-20">
+        <section className="paddings w-full">
+          <div className="flex-center relative min-h-[274px] w-full flex-col rounded-xl bg-black-400 bg-cover bg-center text-center">
+            <h1 className="sm:heading1 heading2 mb-6 text-center text-green-500">Thermo</h1>
+          </div>
+        </section>
+      </div>
+      
+      <div className="paddings">
+        <div className="bg-yellow-300 rounded-xl pt-5">
+          <div className="px-24 py-5">
+            <h1 className='text-2xl underline'>Home Child 1</h1>
+            <div className="flex gap-3 flex-col">
+              <Button className='bg-blue-400'>1-Click Me</Button>
+              <Button className='bg-red-400'>2-Click Me</Button>
+              <Button className='bg-green-400'>3-Click Me</Button>
+              <Button className='bg-purple-400'>4-Click Me</Button>
+            </div>
+
+            <h1 className='text-2xl underline pt-5'>Home Child 2</h1>
+            <div className="grid gap-3 grid-rows-2 grid-flow-col">
+              <Button className='bg-blue-400'>1-Click Me</Button>
+              <Button className='bg-red-400'>2-Click Me</Button>
+              <Button className='bg-green-400'>3-Click Me</Button>
+              <Button className='bg-purple-400'>4-Click Me</Button>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
+
+    </main>
   )
 }
