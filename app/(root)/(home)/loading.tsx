038c85a583e5cb99a5155c60f8dd58e8e427c399
Wilson Chan <wilsonchan54@gmail.com>
2023-10-19 18:28:59 -0400

    Imported shadcn-ui skeleton loading animation / created loading.tsx to render loading animation

--- app/(root)/(home)/loading.tsx
+++ app/(root)/(home)/loading.tsx
@@ -1,10 +1,21 @@
-import React from 'react'
+import { Skeleton } from "@/components/ui/skeleton"
 
 type Props = {}
 
 const loading = (props: Props) => {
   return (
-    <div>loading</div>
+    <main className="flex-center flex-col paddings mx-auto w-full max-w-screen-2xl">    
+      <section className="nav-padding w-full">
+        <Skeleton className="w-[100px] h-[20px] rounded-full bg-blue-600" />
+      </section>
+
+      <section className="mt-6 flex flex-col w-full sm:mt-20">
+        <Skeleton className="h-10 w-56"/>
+        <div className="mt-12 flex flex-wrap w-full justify-center gap-16 sm:justify-start">
+          <Skeleton className="h-[440px] w-56"/>
+        </div>
+      </section>
+    </main>
   )
 }
 
